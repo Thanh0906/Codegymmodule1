24bb21dfd2ff08a493f733b655ccbff72e138f09
Thanh0906 <87293638+Thanh0906@users.noreply.github.com>
2021-07-19 19:11:05 +0700

    aa

---
+++ 07_bien/baitap/dtb học sinh.js
@@ -0,0 +1,11 @@
+<script>
+var inputhoa = prompt('Nhập điểm Hóa');
+var inputli = prompt('Nhập điểm vật lí');
+var inputsinh = prompt('Nhập điểm sinh');
+var hoa = parseInt("inputhoa");
+var li =parseInt('inputli');
+var sinh = parseInt('inputsinh');
+DTB= (hoa+li+sinh)/3;
+Tong= hoa+ li+ sinh;
+alert(F)
+</script>
---
+++ 07_bien/baitap/money.js
@@ -0,0 +1,9 @@
+// lấy giá trị
+var aa = import("VND");
+var bb = import("USD");
+var cc = import("YEN");
+//chuyển đổi sang kiểu number
+aa = parseInt(aa);
+bb = parseInt(bb);
+cc = parseInt(cc);
+if
